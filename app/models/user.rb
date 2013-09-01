f587657e8556d2e6af0137236d5bdc82e02edade
Mitch Seaman <mitchel.seaman@gmail.com>
2013-08-31 23:36:35 -0700

    made provider accessible in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider
   # attr_accessible :title, :body
 
   # validates :email, :presence => { :message => "is required to complete registration." }
